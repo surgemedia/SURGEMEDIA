bc3445053fd83ef544a59830ccda9389d86b45f2
Hector Navarro <hectornucv@gmail.com>
2016-02-25 15:45:57 +1000

    Contact map

--- wp-content/themes/Basetheme/template_contact.php
+++ wp-content/themes/Basetheme/template_contact.php
@@ -44,6 +44,16 @@
 
 
 <section>
-  
+      <?php 
+
+$location = get_field('address');
+
+if( !empty($location) ):
+?>
+<div class="acf-map">
+  <div class="marker" data-lat="<?php echo $location['lat']; ?>" data-lng="<?php echo $location['lng']; ?>"></div>
+</div>
+<?php endif; ?>
+
 </section>
 <?php endwhile; ?>
